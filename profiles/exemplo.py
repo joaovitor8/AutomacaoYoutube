14d68a448ec0697fc23b9c4ba3646dfa90157cbc
João Vitor <joaoezeki@gmail.com>
2026-07-08 19:30:04 -0300

    ...

---
+++ profiles/exemplo.py
@@ -0,0 +1,17 @@
+PERFIL = {
+  "nome": "Nome do Canal",
+  "credentials_file": "credentials/client_secret.json",
+  "token_file": "tokens/token.json",
+  "queue_file": "list/Nome do Canal/agendar.csv",
+  "category_id": "10",  # muda conforme o canal
+  "tags_fixas": [
+    "tag1",
+    "tag2",
+  ],
+  "descricao_fixa": """
+----------
+
+Descrição fixa do Canal aqui...
+"""
+}
+
